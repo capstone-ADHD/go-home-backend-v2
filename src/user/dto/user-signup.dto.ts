32b2afa662d535ee23cd21cf347f93ed0c742784
apdlvmfgkrhtlvek <kimsun050820@gmail.com>
2025-05-19 19:54:48 +0900

    user 잘못건드렸다

--- src/user/dto/user-signup.dto.ts
+++ src/user/dto/user-signup.dto.ts
@@ -1,29 +1,22 @@
-import { ApiProperty } from "@nestjs/swagger";
 import { IsEmail, IsString } from "class-validator";
 
 
 export class UserSignupDto {
-    @ApiProperty({ example: 'capstoneadhd@gmail.com', description: '이메일' })
     @IsEmail()
     email:string;
 
-    @ApiProperty({ example: '12341234', description: '비밀번호' })
     @IsString()
     password:string;
 
-    @ApiProperty({ example: '08대장윤재한', description: '이름' })
     @IsString()
     name:string;
 
-    @ApiProperty({ example: 'f', description: '성별(f/m으로 남자 여자 구분)' })
     @IsString()
     sex:string;
 
-    @ApiProperty({ example: '경북소프트웨어고등학교', description: '학교' })
     @IsString()
     school:string;
 
-    @ApiProperty({ example: 'nh 120124912499', description: '계좌' })
     @IsString()
     account:string;
 }
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -3,7 +3,6 @@ import { UserSignupDto } from "./dto/user-signup.dto";
 import { UserService } from "./user.service";
 import { UserLoginDto } from "./dto/user-login.dto";
 import { JwtAuthGuard } from "../jwt/jwt-auth.guard";
-import { ApiOperation, ApiResponse } from "@nestjs/swagger";
 
 @Controller("user") 
 export class UserController {
@@ -12,8 +11,6 @@ export class UserController {
     ){}
 
     @Post("register")
-    @ApiOperation({ summary: '회원가입', description: '회원가입' })
-    @ApiResponse({ status: 200, description: '성공적으로 회원가입함ㅇㅇ' })
     async Register(@Body() body:UserSignupDto) {
         const res = await this.userService.userRegister(body);
 
@@ -21,19 +18,14 @@ export class UserController {
     }
 
     @Post("login")
-    @ApiOperation({ summary: '로그인', description: '로그인' })
-    @ApiResponse({ status: 200, description: '성공적으로 로그인 함ㅇㅇ' })
     async Login(@Body() body:UserLoginDto) {
         const res = await this.userService.userLogin(body); 
         
         return res;
     }
 
-
     @UseGuards(JwtAuthGuard)
     @Get("profile")
-    @ApiOperation({ summary: '프로필', description: '프로필 정보를 불러옴' })
-    @ApiResponse({ status: 200, description: '성공적으로 프로필을 불러옴' })
     async UserProfile(@Req() req,) {
         const res = await this.userService.userProfile(req.user_name);
 
