42d1d2c3f26e79e2448c6801baa2b645fa0dd80e
jes0131 <wkddmstn0131@gmail.com>
2025-06-25 20:14:22 +0900

    fix: 테스트용 엔드포인트 제거

--- src/chat/chat.controller.ts
+++ src/chat/chat.controller.ts
@@ -7,10 +7,10 @@ import { ApiTags, ApiOperation, ApiResponse } from '@nestjs/swagger';
 export class ChatController {
     constructor(private readonly chatService:ChatService){}
 
-    @Get('push')
-    async testGet() {
-        return await this.chatService.sendPushMultiUser(1, "admin", "test");
-    }
+    // @Get('push')
+    // async testGet() {
+    //     return await this.chatService.sendPushMultiUser(1, "admin", "test");
+    // }
 
     @Get(':room_id')
     @ApiOperation({ summary: '채팅 내역 출력', description: 'room_id에 해당하는 채팅방의 채팅 내역을 출력' })
