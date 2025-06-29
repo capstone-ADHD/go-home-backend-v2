c9e732320d32ed44012005b3523dbf6966b7af50
jes0131 <wkddmstn0131@gmail.com>
2025-06-29 23:18:23 +0900

    fix: user_id 잘못 가져오는 오류 수정

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -36,7 +36,7 @@ export class UserController {
     @ApiOperation({ summary: '프로필', description: '프로필 정보를 불러옴' })
     @ApiResponse({ status: 200, description: '성공적으로 프로필을 불러옴' })
     async UserProfile(@Req() req,) {
-        const res = await this.userService.userProfile(req.user_name);
+        const res = await this.userService.userProfile(req.user.profile.name);
 
         return res;
     }
